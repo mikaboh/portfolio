cee0eccf42b5970e54b34ceba21c47c74af61cae
mikaboh <bohmmika@gmail.com>
2024-06-28 02:57:49 +0200

    add some animations to skills

--- src/components/sections/skills/Skill.tsx
+++ src/components/sections/skills/Skill.tsx
@@ -1,10 +1,56 @@
-function Skill({ name, icon }: SkillProps) {
-    return (
-        <div className="flex flex-col items-center w-20 relative">
-            <span>{icon}</span>
+import { useState } from "react";
 
-            <span className="text-sm text-opacity-70 text-textOnDark absolute top-[70px] whitespace-nowrap">{name}</span>
-        </div>
+function Skill({ name, icon }: SkillProps) {
+    const [isAnimating, setIsAnimating] = useState(false);
+
+    const handleAnimationStart = () => {
+        setIsAnimating(true);
+    };
+
+    const handleAnimationEnd = () => {
+        setIsAnimating(false);
+    };
+
+    const handleMouseEnter = () => {
+        if (!isAnimating) {
+            setIsAnimating(true);
+        }
+    };
+
+    return (
+        <>
+            <style>
+                {`
+                @keyframes spin-and-scale {
+                    0% {
+                        transform: scale(1) rotate(0deg);
+                    }
+                    50% {
+                        transform: scale(1.3);
+                    }
+                    100% {
+                        transform: scale(1) rotate(360deg);
+                    }
+                }
+                .spin-once {
+                    animation: spin-and-scale 0.7s ease-in-out 1;
+                }
+                `}
+            </style>
+            <div
+                className="flex flex-col items-center w-20 relative hover:cursor-pointer"
+                onMouseEnter={handleMouseEnter}
+            >
+                <div className={`transition duration-300 ease-in-out ${isAnimating ? 'spin-once' : ''}`}
+                    onAnimationStart={handleAnimationStart}
+                    onAnimationEnd={handleAnimationEnd}>
+                    {icon}
+                </div>
+                <span className="text-sm text-opacity-70 text-textOnDark absolute top-[70px] whitespace-nowrap">
+                    {name}
+                </span>
+            </div>
+        </>
     );
 }
 
